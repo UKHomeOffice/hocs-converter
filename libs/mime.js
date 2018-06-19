9560df7100b8eb490cfc7f0f3bdab91471770625
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-06-19 09:23:01 +0100

    Recfactor code and implemenet tests

---
+++ libs/mime.js
@@ -0,0 +1,31 @@
+const mimeDb = require('mime-db');
+const mmm = require('mmmagic');
+const magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE);
+const config = require('../config');
+
+const mimeTypes = Object.keys(mimeDb).reduce((reducer, key) => {
+    const type = mimeDb[key];
+    if (type.extensions && type.extensions.length > 0) {
+        type.extensions.map(extension => reducer[extension] = key);
+    }
+    return reducer;
+}, {});
+
+const supported_types = (process.env.SUPPORTED_TYPES || config.SUPPORTED_TYPES).split(',');
+
+const supported_mime_types = supported_types.reduce((reducer, extension) => {
+    reducer.push(mimeTypes[extension]);
+    return reducer;
+}, []);
+
+const detectFile = (file, callback) => {
+    magic.detectFile(file, (err, result) => {
+        callback(err, result);
+    })
+};
+
+module.exports = {
+    supported_types,
+    supported_mime_types,
+    detectFile
+};
